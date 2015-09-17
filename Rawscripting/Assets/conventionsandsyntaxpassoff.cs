03c73de3d9d24c07c9caf08a0c5edf4a8a196958
Cameron Stack or CS <rawmangom@gmail.com>
2015-09-17 09:10:59 -0600

    no message

--- Rawscripting/Assets/conventionsandsyntaxpassoff.cs
+++ Rawscripting/Assets/conventionsandsyntaxpassoff.cs
@@ -14,13 +14,13 @@ public class conventionsandsyntaxpassoff : MonoBehaviour
 	void Update()
 	{
 		//. The following line of code is used to track the position on the falling cube. Enter the correct convention.//. 
-		Debug.Log (transform.position.y);
+		Debug.Log (transform.position.x);
 
-		//. The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10
-		//. Enter the correct statement //. 
-		if (transform.position.y <= 10)
-		{
+		 //. The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10
+		 //. Enter the correct statement //. 
+		 if (transform.position.y <= 10)
+		 {
 			Debug.Log ("I'm going to splater!");
-		}
+		 }
 	}
 }
