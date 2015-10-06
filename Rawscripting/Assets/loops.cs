85301117089aa33e6f62dc9c943acd2ad6627800
Cameron Stack or CS <rawmangom@gmail.com>
2015-10-06 09:39:22 -0600

    loops

---
+++ Rawscripting/Assets/loops.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loops : MonoBehaviour {
+
+			int shirtsonthefloor = 9;
+			
+			void Start ()
+			{
+				while(shirtsonthefloor > 0)
+				{
+					Debug.Log ("I've washed one of the shirts!");
+					shirtsonthefloor--;
+				}
+			}
+		}	
---
+++ Rawscripting/Assets/loops3.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loops3 : MonoBehaviour 
+	{
+		int numfriends = 7;
+		
+		
+		void Start ()
+		{
+			for(int i = 0; i < numfriends; i++)
+			{
+				Debug.Log("Creating friends number: " + i);
+			}
+		}
+	}
+
+
