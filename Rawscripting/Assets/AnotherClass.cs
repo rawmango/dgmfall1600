17705b18c2f09d9a8bb8fb94dbce66987f381ab7
Cameron Stack or CS <rawmangom@gmail.com>
2015-10-08 09:34:15 -0600

    no message

---
+++ Rawscripting/Assets/AnotherClass.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass : MonoBehaviour 
+{
+	
+	public int sunglasses;
+	public int earpiece;
+	
+	
+	private int gun;
+	private int intel;
+
+	
+	public void agentMachine (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("agent total: " + answer);
+	}
+	
+	
+	private void spySort (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("spy total: " + answer);
+	}
+}
