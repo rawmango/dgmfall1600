ae547740ed8763f4ac27b7273528448e0d0dfd31
Cameron Stack or CS <rawmangom@gmail.com>
2015-11-03 09:51:36 -0700

    switches.

--- Rawscripting/Assets/Enum.cs
+++ Rawscripting/Assets/Enum.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Enum : MonoBehaviour 
 	{
-		enum Direction{Orem=27, Lehi =13, Payson =7, Provo=19, };void Start () 
+		enum Direction{Orem=27, Lehi=13, Payson=7, Provo=19, };void Start () 
 		{
 			Direction myDirection;
 			
@@ -12,16 +12,23 @@ public class Enum : MonoBehaviour
 		
 		Direction ReverseDirection (Direction dir)
 		{
-			if(dir == Direction.Orem )
-				dir = Direction.Lehi;
-			else if(dir == Direction.Lehi)
-				dir = Direction.Orem;
-			else if(dir == Direction.Payson)
-				dir = Direction.Provo;
-			else if(dir == Direction.Provo)
-				dir = Direction.Payson;
-			
-			return dir;   
-		debug.log 
-		}
+		if (dir == Direction.Orem)
+			dir = Direction.Lehi;
+		else if (dir == Direction.Lehi)
+			dir = Direction.Orem;
+		else if (dir == Direction.Payson)
+			dir = Direction.Provo;
+		else if (dir == Direction.Provo)
+			dir = Direction.Payson;
+
+		//return dir
+
 	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			Debug.Log("Direction" );
+			
+	 }
+}
---
+++ Rawscripting/Assets/switchstates.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchstates : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	{
+		public int iceintel = 5;
+		
+		
+		void Greet()
+		{
+			switch (iceintel)
+			{
+			case 5:
+				print (" hey there! Let me teach you thing or two!");
+				break;
+			case 4:
+				print ("deaddrop!");
+				break;
+			case 3:
+				print ("?");
+				break;
+			case 2:
+				print ("Grog SMASH!");
+				break;
+			case 1:
+				print ("Ulg, glib, Pblblblblb");
+				break;
+			default:
+				print ("Incorrect intelligence level.");
+				break;
+			}
+		}
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
