74b20f3a7ad07375c4e693f39b10743f44d060c1
Cameron Stack <InterAmerica@Camerons-Air.home>
2015-12-14 01:44:00 -0700

    Tank Turret

---
+++ Rawscripting/Assets/bulletss.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletss : MonoBehaviour {
+
+	void  Update (){
+		
+		if(Input.GetKey("x")){
+			transform.Rotate(0,0,-1);
+		}
+		
+		if(Input.GetKey("v")){
+			transform.Rotate(0,0,1);
+		}
+		
+	}
+}
