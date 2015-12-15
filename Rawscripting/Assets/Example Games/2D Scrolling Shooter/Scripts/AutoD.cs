9791028c83fd18ad1fefe0cf153f2681e4dc4628
Cameron Stack <InterAmerica@Camerons-Air.ad.uvu.edu>
2015-12-15 07:35:33 -0700

    touch ups

--- Rawscripting/Assets/Example Games/2D Scrolling Shooter/Scripts/AutoD.cs
+++ Rawscripting/Assets/Example Games/2D Scrolling Shooter/Scripts/AutoD.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class AutoD : MonoBehaviour {
 		void OnTriggerEnter(Collider other) {
-			if (other.CompareTag("GI"))
+			if (other.CompareTag("soldier"))
 				Destroy(other.gameObject);
-
-		}
-	}
-//	GameObject delay= 20.0f; //This implies a delay of 2 seconds.
+		float delay= 20.0f; }
+}//This implies a delay of 2 seconds.
 	
 	//  WaitAndDestroy (){
 	//	yield return new WaitForSeconds(delay);
