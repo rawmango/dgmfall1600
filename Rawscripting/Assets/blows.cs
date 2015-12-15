437028827633346de607061da3c9fc9b356c675a
Cameron Stack <InterAmerica@Camerons-Air.home>
2015-12-14 21:00:13 -0700

    blow

---
+++ Rawscripting/Assets/blows.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blows : MonoBehaviour {
+
+	void  OnCollisionEnter ( Collision collision  ){ 
+
+		if(collision.gameObject.name==("Bullets(Clone)")) 
+			
+		{ 
+			
+			// Instantiate explosion at the impact point and rotate the explosion 
+			// so that the y-axis faces along the surface normal 
+			
+			
+			ContactPoint contact = collision.contacts[0]; 
+		Quaternion rotation= Quaternion.FromToRotation(Vector3.up, contact.normal); 
+			
+			// And kill our selves 
+			Kill (); 
+		} 
+		
+	}
+	
+	void  Kill (){ 
+		
+		
+		// Destroy the projectile 
+		Destroy(gameObject); 
+	}
+}
