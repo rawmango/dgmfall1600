b026744e55eb9e27363a23b593ed6430aa9c9505
Cameron Stack <InterAmerica@Camerons-Air.home>
2015-12-15 03:20:19 -0700

    no message

--- Rawscripting/Assets/CustomizableGun_FreeED/Scripts/Blow.cs
+++ Rawscripting/Assets/CustomizableGun_FreeED/Scripts/Blow.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class Blow : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+	function OnCollisionEnter (collision : Collision) {
+		// Instantiate explosion at the impact point and rotate the explosion
+		// so that the y-axis faces along the surface normal
+		var contact : ContactPoint = collision.contacts[0];
+		var rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		
+		if (collision.gameObject.tag =="Enemy"){
+			// And kill our selves
+			Kill ();
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	function Kill () {
+		
+		
+		// Destroy the projectile
+		Destroy(gameObject);
 	}
-}
+	
+	
+	script RequireComponent (Rigidbody)
---
+++ Rawscripting/Assets/Example Games/2D Scrolling Shooter/Scripts/AutoD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoD : MonoBehaviour {
+	float life;
+	private float destroyTime;
+	
+	void  Start (){
+		destroyTime = Time.time + life;
+	}
+	
+	void  Update (){
+		if(Time.time > destroyTime){
+			Destroy(gameObject);
+		}
+		if(Time.time > destroyTime - particleEmitter.maxEnergy){
+			particleEmitter.emit = false;
+		}
+		
+	}
+
+}
