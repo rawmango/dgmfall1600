4e14febab9d16261e58a8017586d540ff1b40535
Cameron Stack <InterAmerica@Camerons-Air.home>
2015-12-15 02:58:58 -0700

    targets

---
+++ Rawscripting/Assets/SelfD.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfD : MonoBehaviour {
+	float life;
+	private float destroyTime;
+	
+	void  Start (){
+		destroyTime = Time.time + life;
+	}
+	
+	void  Update (){
+		if(Time.time > destroyTime){
+			Destroy(gameObject);
+		}
+	}
+}
